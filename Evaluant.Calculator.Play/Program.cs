82371ab0f00fb74cca38fc3a586e4b4c8fb955cf
wanglj <wanglj2007@qq.com>
2022-09-16 14:43:37 +0800

    add param

--- Evaluant.Calculator.Play/Program.cs
+++ Evaluant.Calculator.Play/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 
 namespace NCalc.Play
 {
@@ -20,14 +21,24 @@ namespace NCalc.Play
 				"3 > 2 and 1 <= (3-2)",
 				"3 % 2 != 10 % 3",
                 "if( [age] >= 18, 'majeur', 'mineur')",
-                "CalculateBenefits([user]) * [Taxes]"
+				//"CalculateBenefits([user]) * [Taxes]"
 			};
 
-            foreach (string expression in expressions)
-				Console.WriteLine("{0} = {1}", 
-					expression, 
-					new Expression(expression).Evaluate());
 
+
+			foreach (string expression in expressions)
+			{
+                var exp = new Expression(expression);
+				exp.EvaluateParameter += (s,e) => {
+					if (s == "age") e.Result = 8;
+				};
+
+                Console.WriteLine("{0} = {1}",
+					expression,
+					exp.Evaluate());
+			}
+
+			Console.ReadKey();
 		}
 	}
 }
